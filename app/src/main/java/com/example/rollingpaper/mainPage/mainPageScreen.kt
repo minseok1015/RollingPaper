fe162e8dee9ce3f7f6217b15f81743882265339a
김민석 <skyfox001015@naver.com>
2024-05-25 16:35:05 +0900

    feat : 한줄에 카드 두개, 좋아요추가(클릭은 안됨)

--- app/src/main/java/com/example/rollingpaper/mainPage/mainPageScreen.kt
+++ app/src/main/java/com/example/rollingpaper/mainPage/mainPageScreen.kt
@@ -1,5 +1,6 @@
 package com.example.rollingpaper.mainPage
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,22 +8,26 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -34,7 +39,7 @@ import kotlin.random.Random
 fun MainPageScreen() {
     Column {
         val memoModel = viewModel<MemoViewModel>()
-        val chunkedItems = memoModel.memoList.chunked(3)
+        val chunkedItems = memoModel.memoList.chunked(2)
 
         LazyColumn(
             contentPadding = PaddingValues(16.dp),
@@ -46,10 +51,12 @@ fun MainPageScreen() {
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
                     for (item in rowItems) {
-                        MemoItem(MemoContents = item, modifier = Modifier.weight(1f))
+                        MemoItem(MemoContents = item, modifier = Modifier.weight(1f)) {
+                            // onClick 핸들러
+                        }
                     }
-                    if (rowItems.size < 3) {
-                        for (i in rowItems.size until 3) {
+                    if (rowItems.size < 2) {
+                        for (i in rowItems.size until 2) {
                             Spacer(modifier = Modifier.weight(1f))
                         }
                     }
@@ -61,37 +68,48 @@ fun MainPageScreen() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MemoItem(MemoContents: Memo, modifier: Modifier = Modifier) {
+fun MemoItem(MemoContents: Memo, modifier: Modifier = Modifier, onClick: () -> Unit) {
     val rotationAngle = Random.nextFloat() * 10 - 5 // -5도에서 5도 사이의 랜덤 각도
 
     Box(modifier = modifier.padding(8.dp)) {
-        Card(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(150.dp)
-                .graphicsLayer(rotationZ = rotationAngle),
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                .graphicsLayer(rotationZ = rotationAngle)
         ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                verticalArrangement = Arrangement.SpaceBetween
+            Card(
+                modifier = Modifier.fillMaxSize(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
             ) {
-                Text(text = MemoContents.content)
-                Text(text = MemoContents.name)
-            }
-        }
-        BadgedBox(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .offset(x = (-8).dp, y = 8.dp),
-            badge = {
-                Badge {
-                    Text(text = "❤️", style = MaterialTheme.typography.bodySmall)
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = MemoContents.content)
+                    Text(text = MemoContents.name)
                 }
             }
-        ) {}
+            BadgedBox(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .offset(x = (-8).dp, y = 8.dp),
+                badge = {
+                    Badge {
+                        Text(text = "${MemoContents.like}")
+                    }
+                }
+            ) {
+                Icon(
+                    Icons.Default.Favorite,
+                    contentDescription = null,
+                    tint = Color.Red,
+                    modifier = Modifier.clickable { onClick() }
+                )
+            }
+        }
     }
 }
